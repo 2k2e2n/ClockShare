bd39c29bb0cde0e28b9c0df753206d4f1953aa42
2k2e2n <wata3ken10@gmail.com>
2024-06-24 01:54:13 +0900

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
+const prefixPath = !isProd ? '/sub-derectory' : ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-
+    assetPrefix: prefixPath,
+    basePath: prefixPath,
 };
 
 export default nextConfig;
