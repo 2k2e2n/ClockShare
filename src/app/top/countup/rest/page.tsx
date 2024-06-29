1ddab87eb92f5117354ea92ec868d35fd5d5cb5f
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2024-06-29 20:57:57 +0900

    commit

--- src/app/top/countup/rest/page.tsx
+++ src/app/top/countup/rest/page.tsx
@@ -18,48 +18,53 @@ import Clock from '@/components/Clock';
 
 export default function Page() {
     let [time, settime] = useState(30);          //タイマー
-    let [timeS, settimeS] = useState<number>(0); //秒
-    let [timeM, settimeM] = useState<number>(0); //分
-    let [timeH, settimeH] = useState<number>(0); //時
+
 
     let [resttime, setresttime] = useState<number>(10);  //タイマー
-    let [resttimeS, setresttimeS] = useState<number>(0); //秒
-    let [resttimeM, setresttimeM] = useState<number>(0); //分
-    let [resttimeH, setresttimeH] = useState<number>(0); //時
 
     useEffect(() => {
         let localKey : number = Number(getlocalKey('time'));
         settime(localKey);
-        settimeS(localKey % 60);
-        settimeM(Math.floor(localKey / 60) % 60);
-        settimeH(Math.floor(localKey / (60*60)) % 24);
         const intervalId = window.setInterval(countdown, 1000);  //１秒に１回実行
         return () => clearInterval(intervalId);  // クリーンアップ
-
     }, []);
+
     function countdown() {
         setresttime((resttime) => {
-            const newTime = resttime;
-            if(resttime > 0) {
-                resttime = resttime - 1;
-                if(resttime == 1) {
-                    setTimeout(function() {
-                        toast(
-                            '休憩が終わりました！！！勉強しよう！'
-                        );
-                      }, 2000);
-                }
-            } else {
+            if(resttime <=  0) {
                 resttime = 0;
-            }
-            setresttime(resttime);
-            setresttimeS(resttime % 60);
-            setresttimeM(Math.floor(resttime / 60) % 60);
-            setresttimeH(Math.floor(resttime / (60*60)) % 24);
-            return newTime;
+            } if(resttime == 1) {
+                setTimeout(function() {
+                    toast(
+                        '休憩が終わりました！！！勉強しよう！'
+                    );
+                }, 2000);
+                
+            } else {
+                resttime = resttime - 1;
+            } 
+            return resttime;
         });
-    }
 
+
+
+    }
+    /*
+    const newTime = resttime;
+    if(resttime > 0) {
+        resttime = resttime - 1;
+        if(resttime == 1) {
+            setTimeout(function() {
+                toast(
+                    '休憩が終わりました！！！勉強しよう！'
+                );
+            }, 2000);
+        }
+    } else {
+        resttime = 0;
+    }
+    setresttime(resttime);
+*/
 
     //今の時間の取得
     function getlocalKey(key: string): string {
@@ -109,13 +114,14 @@ export default function Page() {
             <h1 className="text-6xl">top/countup/rest</h1>
             <h1>休憩時間です！</h1>
             <h1 className="text-4xl flex justify-center mt-4">
-               time: <Clock time={time} />
+                time: <Clock time={time} />
             </h1>
             <h1 className="text-6xl flex justify-center mt-4">
-                {`Resttime:${padTime(resttimeH)}:${padTime(resttimeM)}:${padTime(resttimeS)}`}
+                Resttime:<Clock time={resttime} />
             </h1>
             <div className="flex justify-center items-center gap-16 pt-16 mr-32 ml-32">
             <button onClick={() =>{resumebtn('../countup')}} className="defaultbtn">RESUME</button>
+
             </div>
             
         </div>
